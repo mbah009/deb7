d029074b084564e351a6b7acc5eeea5cff7849c3
mbah009 <tmoe@gmx.com>
2017-12-30 15:40:10 +0800

    Update autokill.sh

--- autokill.sh
+++ autokill.sh
@@ -9,6 +9,16 @@ while :
   sleep 5
   done
 while :
+  do 
+  userlimitssh.sh 1 
+  sleep 5 
+  done
+while :
+  do
+  userlimitssh.sh 2
+  sleep 5
+  done
+while :
   do
   userexpired.sh
   sleep 36000
