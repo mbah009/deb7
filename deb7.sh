6537230d50cbdbb773d1a4732cedd65f93346add
mbah009 <tmoe@gmx.com>
2017-12-31 20:22:42 +0800

    Update deb7.sh

--- deb7.sh
+++ deb7.sh
@@ -123,6 +123,7 @@ cd
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_BANNER=""/DROPBEAR_BANNER="\/etc\/banner"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 
